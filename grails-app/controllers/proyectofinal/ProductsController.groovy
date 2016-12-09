3d700caeacab6932f291cbbeb75e37f9e47e426e
EdmilDM <edmildm@gmail.com>
2016-12-09 02:20:47 -0400

    Arreglando errores

--- grails-app/controllers/proyectofinal/ProductsController.groovy
+++ grails-app/controllers/proyectofinal/ProductsController.groovy
@@ -1,6 +1,10 @@
 package proyectofinal
 
 import grails.plugin.springsecurity.annotation.Secured
+import grails.transaction.Transactional
+
+import static org.springframework.http.HttpStatus.CREATED
+import static org.springframework.http.HttpStatus.OK
 
 @Secured( [ "ROLE_USER" ] )
 class ProductsController {
@@ -22,5 +26,62 @@ class ProductsController {
     def view( ){
         Product product = Product.get( params.id )
         [ product: product ]
+    }
+
+    def create() {
+        respond new Product(params)
+    }
+
+    def edit(Product product) {
+        respond product
+    }
+
+    @Transactional
+    def update(Product product) {
+        if (product == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (product.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond user.errors, view:'edit'
+            return
+        }
+
+        product.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'product.label', default: 'User'), user.id])
+                redirect index()
+            }
+            '*'{ respond product, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def save(Product product) {
+        if (product == null) {
+                transactionStatus.setRollbackOnly()
+                notFound()
+                return
+            }
+
+        if (product.hasErrors()) {
+                transactionStatus.setRollbackOnly()
+                respond product.errors, view:'create'
+                return
+        }
+
+        product.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                redirect index()
+            }
+            '*' { respond product, [status: CREATED] }
+        }
     }
 }
